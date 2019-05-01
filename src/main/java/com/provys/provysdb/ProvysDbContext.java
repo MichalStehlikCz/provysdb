178738e1570947d00a680c4becc0c0eace1715f1
MichalStehlikCz <michal.stehlik.cz@gmail.com>
2019-05-01 14:06:44 +0200

    When connection with token fails, original exception is not set as cause

--- src/main/java/com/provys/provysdb/ProvysDbContext.java
+++ src/main/java/com/provys/provysdb/ProvysDbContext.java
@@ -12,6 +12,7 @@ import org.jooq.impl.DefaultConfiguration;
 import javax.annotation.Nonnull;
 import javax.enterprise.context.ApplicationScoped;
 import java.sql.SQLException;
+import java.util.Objects;
 
 /**
  * Class supports connection to Provys database and gives access to this database either via datasource or via JOOQ
@@ -80,9 +81,12 @@ public class ProvysDbContext {
     @Nonnull
     public DSLContext createDSL(String dbToken) {
         try {
-            return DSL.using(buildNoDSJooqConfiguration().set(provysDataSource.getConnectionWithToken(dbToken)));
+            return DSL.using(buildNoDSJooqConfiguration().set(provysDataSource.getConnectionWithToken(
+                    Objects.requireNonNull(dbToken))));
         } catch (SQLException e) {
-            throw new RegularException(LOG, "PROVYSDB_CANNOTCONNECTWITHTOKEN", "Failed to initialize connection with token");
+            // we do not want to display token in logs for security reasons
+            throw new RegularException(LOG, "PROVYSDB_CANNOTCONNECTWITHTOKEN",
+                    "Failed to initialize connection with token", e);
         }
     }
 
