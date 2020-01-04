3fc3538019c0eca65c7d47b83a6250bb6c089ece
MichalStehlikCz <michal.stehlik.cz@gmail.com>
2020-01-04 12:09:25 +0100

    Started migration to Spring framework

--- dbcontext/src/main/java/com/provys/provysdb/dbcontext/impl/ProvysDbConfiguration.java
+++ dbcontext/src/main/java/com/provys/provysdb/dbcontext/impl/ProvysDbConfiguration.java
@@ -1,79 +1,36 @@
 package com.provys.provysdb.dbcontext.impl;
 
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-
 import javax.annotation.Nonnull;
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-import java.util.Objects;
 
 /**
- * Class retrieves parameters of database connection from environment. Used by {@code ProvysConnectionPoolDataSource}
- * as source of properties for establishing connection
+ * Interface represents configuration needed for datasource initiation.
  */
-@ApplicationScoped
-class ProvysDbConfiguration {
-
-    @Inject
-    @ConfigProperty(name = "provysdb.url")
-    private String url;
-
-    @Inject
-    @ConfigProperty(name = "provysdb.user")
-    private String user;
-
-    @Inject
-    @ConfigProperty(name = "provysdb.pwd")
-    private String pwd;
-
-    @Inject
-    @ConfigProperty(name = "provysdb.minpoolsize", defaultValue = "1")
-    private int minPoolSize;
-
-    @Inject
-    @ConfigProperty(name = "provysdb.maxpoolsize", defaultValue = "10")
-    private int maxPoolSize;
-
+interface ProvysDbConfiguration {
     /**
      * @return value of field url
      */
     @Nonnull
-    public String getUrl() {
-        return Objects.requireNonNull(url, "Property provysdb.url not specified");
-    }
+    String getUrl();
 
     /**
      * @return value of field user
      */
     @Nonnull
-    public String getUser() {
-        return Objects.requireNonNull(user, "Property provysdb.user not specified");
-    }
+    String getUser();
 
     /**
      * @return value of field pwd
      */
-    public String getPwd() {
-        return Objects.requireNonNull(pwd, "Property provysdb.pwd not specified");
-    }
+    @Nonnull
+    String getPwd();
 
     /**
      * @return value of field minPoolSize
      */
-    public int getMinPoolSize() {
-        if (minPoolSize <= 0) {
-            throw new IllegalArgumentException("Invalid value of property provysdb.minpoolsize " + minPoolSize);
-        }
-        return minPoolSize;
-    }
+    int getMinPoolSize();
 
     /**
      * @return value of field maxPoolSize
      */
-    public int getMaxPoolSize() {
-        if (maxPoolSize <= 0) {
-            throw new IllegalArgumentException("Invalid value of property provysdb.maxpoolsize " + maxPoolSize);
-        }
-        return maxPoolSize;
-    }
+    int getMaxPoolSize();
 }
