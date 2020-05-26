4854d640b672941687e0c878fd31987d0f791095
MichalStehlikCz <michal.stehlik.cz@gmail.com>
2020-05-26 09:48:32 +0200

    Support for User context based query builder

--- provysquery/src/main/java/com/provys/db/provysquery/ProvysQueryFactory.java
+++ provysquery/src/main/java/com/provys/db/provysquery/ProvysQueryFactory.java
@@ -2,11 +2,13 @@ package com.provys.db.provysquery;
 
 import com.google.errorprone.annotations.Immutable;
 import com.provys.db.provysdb.AdminDbContext;
+import com.provys.db.provysdb.UserDbContext;
 import com.provys.db.querybuilder.ElementBuilderFactory;
 import com.provys.db.sqlquery.query.DefaultStatementFactory;
 import com.provys.db.sqlquery.query.StatementFactory;
 import com.provys.db.sqlquerybuilder.AdminQueryBuilderFactory;
 import com.provys.db.sqlquerybuilder.DefaultSqlQueryBuilderFactory;
+import com.provys.db.sqlquerybuilder.UserQueryBuilderFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,14 +20,25 @@ import org.springframework.context.annotation.Configuration;
 @Immutable
 public class ProvysQueryFactory {
 
-  @Bean("AdminStatementFactory")
+  @Bean("adminStatementFactory")
   StatementFactory adminStatementFactory(AdminDbContext dbContext) {
     return new DefaultStatementFactory(dbContext);
   }
 
   @Bean
   AdminQueryBuilderFactory adminQueryBuilderFactory(
-      @Qualifier("AdminStatementFactory") StatementFactory statementFactory) {
+      @Qualifier("adminStatementFactory") StatementFactory statementFactory) {
+    return new DefaultSqlQueryBuilderFactory(statementFactory, ElementBuilderFactory.getInstance());
+  }
+
+  @Bean("userStatementFactory")
+  StatementFactory userStatementFactory(UserDbContext dbContext) {
+    return new DefaultStatementFactory(dbContext);
+  }
+
+  @Bean
+  UserQueryBuilderFactory userQueryBuilderFactory(
+      @Qualifier("userStatementFactory") StatementFactory statementFactory) {
     return new DefaultSqlQueryBuilderFactory(statementFactory, ElementBuilderFactory.getInstance());
   }
 }
