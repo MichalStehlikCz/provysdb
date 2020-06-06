c881abea0c0a54a8ee21a802de8ce980752cfa83
MichalStehlikCz <michal.stehlik.cz@gmail.com>
2020-06-06 10:21:06 +0200

    Fixed formatting

--- provysdb/src/test/java/com/provys/db/provysdb/ProvysConnectionLabelingCallbackTest.java
+++ provysdb/src/test/java/com/provys/db/provysdb/ProvysConnectionLabelingCallbackTest.java
@@ -12,64 +12,92 @@ import java.util.stream.Stream;
 
 class ProvysConnectionLabelingCallbackTest {
 
-    @SuppressWarnings("squid:S3878")
-    static Stream<Object[]> costTest() {
-        return Stream.of(
-                new Object[]{"", "", ProvysConnectionLabelingCallback.EXACT_MATCH}
-                , new Object[]{"", ProvysConnectionLabelingCallback.PROPERTY_TYPE + '=' + ProvysConnectionLabelingCallback.CONNECTION_GENERIC, ProvysConnectionLabelingCallback.GENERIC_CONNECTION}
-                , new Object[]{"", ProvysConnectionLabelingCallback.PROPERTY_TYPE + '=' + ProvysConnectionLabelingCallback.CONNECTION_TOKEN + '\n'
-                        + ProvysConnectionLabelingCallback.PROPERTY_TOKEN + "=xsqjkhfweqiufhnejngfv\n"
-                        + ProvysConnectionLabelingCallback.PROPERTY_USER + "=1000000000000098", ProvysConnectionLabelingCallback.REUSE_TOKEN}
-                , new Object[]{"", ProvysConnectionLabelingCallback.PROPERTY_TYPE + '=' + ProvysConnectionLabelingCallback.CONNECTION_USER + '\n'
-                        + ProvysConnectionLabelingCallback.PROPERTY_USER + "=1000000000000098", ProvysConnectionLabelingCallback.REUSE_USER}
-                , new Object[]{ProvysConnectionLabelingCallback.PROPERTY_TOKEN + "=xsqjkhfweqiufhnejngfv", "", ProvysConnectionLabelingCallback.NEW_CONNECTION}
-                , new Object[]{ProvysConnectionLabelingCallback.PROPERTY_TOKEN + "=xsqjkhfweqiufhnejngfv", ProvysConnectionLabelingCallback.PROPERTY_TYPE + '='
-                + ProvysConnectionLabelingCallback.CONNECTION_GENERIC, ProvysConnectionLabelingCallback.REUSE_GENERIC}
-                , new Object[]{ProvysConnectionLabelingCallback.PROPERTY_TOKEN + "=xsqjkhfweqiufhnejngfv", ProvysConnectionLabelingCallback.PROPERTY_TYPE + '='
+  @SuppressWarnings("squid:S3878")
+  static Stream<Object[]> costTest() {
+    return Stream.of(
+        new Object[]{"", "", ProvysConnectionLabelingCallback.EXACT_MATCH}
+        , new Object[]{"", ProvysConnectionLabelingCallback.PROPERTY_TYPE + '='
+            + ProvysConnectionLabelingCallback.CONNECTION_GENERIC,
+            ProvysConnectionLabelingCallback.GENERIC_CONNECTION}
+        , new Object[]{"", ProvysConnectionLabelingCallback.PROPERTY_TYPE + '='
+            + ProvysConnectionLabelingCallback.CONNECTION_TOKEN + '\n'
+            + ProvysConnectionLabelingCallback.PROPERTY_TOKEN + "=xsqjkhfweqiufhnejngfv\n"
+            + ProvysConnectionLabelingCallback.PROPERTY_USER + "=1000000000000098",
+            ProvysConnectionLabelingCallback.REUSE_TOKEN}
+        , new Object[]{"", ProvysConnectionLabelingCallback.PROPERTY_TYPE + '='
+            + ProvysConnectionLabelingCallback.CONNECTION_USER + '\n'
+            + ProvysConnectionLabelingCallback.PROPERTY_USER + "=1000000000000098",
+            ProvysConnectionLabelingCallback.REUSE_USER}
+        ,
+        new Object[]{ProvysConnectionLabelingCallback.PROPERTY_TOKEN + "=xsqjkhfweqiufhnejngfv", "",
+            ProvysConnectionLabelingCallback.NEW_CONNECTION}
+        , new Object[]{ProvysConnectionLabelingCallback.PROPERTY_TOKEN + "=xsqjkhfweqiufhnejngfv",
+            ProvysConnectionLabelingCallback.PROPERTY_TYPE + '='
+                + ProvysConnectionLabelingCallback.CONNECTION_GENERIC,
+            ProvysConnectionLabelingCallback.REUSE_GENERIC}
+        , new Object[]{ProvysConnectionLabelingCallback.PROPERTY_TOKEN + "=xsqjkhfweqiufhnejngfv",
+            ProvysConnectionLabelingCallback.PROPERTY_TYPE + '='
                 + ProvysConnectionLabelingCallback.CONNECTION_TOKEN + '\n'
-                        + ProvysConnectionLabelingCallback.PROPERTY_TOKEN + "=xsqjkhfweqiufhnejngfv\n"
-                        + ProvysConnectionLabelingCallback.PROPERTY_USER + "=1000000000000098", ProvysConnectionLabelingCallback.TOKEN_MATCH}
-                , new Object[]{ProvysConnectionLabelingCallback.PROPERTY_TOKEN + "=mdfnwseijtfhuie", ProvysConnectionLabelingCallback.PROPERTY_TYPE + '='
+                + ProvysConnectionLabelingCallback.PROPERTY_TOKEN + "=xsqjkhfweqiufhnejngfv\n"
+                + ProvysConnectionLabelingCallback.PROPERTY_USER + "=1000000000000098",
+            ProvysConnectionLabelingCallback.TOKEN_MATCH}
+        , new Object[]{ProvysConnectionLabelingCallback.PROPERTY_TOKEN + "=mdfnwseijtfhuie",
+            ProvysConnectionLabelingCallback.PROPERTY_TYPE + '='
                 + ProvysConnectionLabelingCallback.CONNECTION_TOKEN + '\n'
-                        + ProvysConnectionLabelingCallback.PROPERTY_TOKEN + "=xsqjkhfweqiufhnejngfv\n"
-                        + ProvysConnectionLabelingCallback.PROPERTY_USER + "=1000000000000098", ProvysConnectionLabelingCallback.REUSE_TOKEN}
-                , new Object[]{ProvysConnectionLabelingCallback.PROPERTY_TOKEN + "=xsqjkhfweqiufhnejngfv", ProvysConnectionLabelingCallback.PROPERTY_TYPE + '='
+                + ProvysConnectionLabelingCallback.PROPERTY_TOKEN + "=xsqjkhfweqiufhnejngfv\n"
+                + ProvysConnectionLabelingCallback.PROPERTY_USER + "=1000000000000098",
+            ProvysConnectionLabelingCallback.REUSE_TOKEN}
+        , new Object[]{ProvysConnectionLabelingCallback.PROPERTY_TOKEN + "=xsqjkhfweqiufhnejngfv",
+            ProvysConnectionLabelingCallback.PROPERTY_TYPE + '='
                 + ProvysConnectionLabelingCallback.CONNECTION_USER + '\n'
-                        + ProvysConnectionLabelingCallback.PROPERTY_USER + "=1000000000000098", ProvysConnectionLabelingCallback.REUSE_USER}
-                , new Object[]{ProvysConnectionLabelingCallback.PROPERTY_USER + "=1000000000000098", "", ProvysConnectionLabelingCallback.NEW_CONNECTION}
-                , new Object[]{ProvysConnectionLabelingCallback.PROPERTY_USER + "=1000000000000098", ProvysConnectionLabelingCallback.PROPERTY_TYPE + '='
-                + ProvysConnectionLabelingCallback.CONNECTION_GENERIC, ProvysConnectionLabelingCallback.REUSE_GENERIC}
-                , new Object[]{ProvysConnectionLabelingCallback.PROPERTY_USER + "=1000000000000098", ProvysConnectionLabelingCallback.PROPERTY_TYPE + '='
+                + ProvysConnectionLabelingCallback.PROPERTY_USER + "=1000000000000098",
+            ProvysConnectionLabelingCallback.REUSE_USER}
+        , new Object[]{ProvysConnectionLabelingCallback.PROPERTY_USER + "=1000000000000098", "",
+            ProvysConnectionLabelingCallback.NEW_CONNECTION}
+        , new Object[]{ProvysConnectionLabelingCallback.PROPERTY_USER + "=1000000000000098",
+            ProvysConnectionLabelingCallback.PROPERTY_TYPE + '='
+                + ProvysConnectionLabelingCallback.CONNECTION_GENERIC,
+            ProvysConnectionLabelingCallback.REUSE_GENERIC}
+        , new Object[]{ProvysConnectionLabelingCallback.PROPERTY_USER + "=1000000000000098",
+            ProvysConnectionLabelingCallback.PROPERTY_TYPE + '='
                 + ProvysConnectionLabelingCallback.CONNECTION_TOKEN + '\n'
-                        + ProvysConnectionLabelingCallback.PROPERTY_TOKEN + "=xsqjkhfweqiufhnejngfv\n"
-                        + ProvysConnectionLabelingCallback.PROPERTY_USER + "=1000000000000098", ProvysConnectionLabelingCallback.USER_MATCH}
-                , new Object[]{ProvysConnectionLabelingCallback.PROPERTY_USER + "=1000000000000098", ProvysConnectionLabelingCallback.PROPERTY_TYPE + '='
+                + ProvysConnectionLabelingCallback.PROPERTY_TOKEN + "=xsqjkhfweqiufhnejngfv\n"
+                + ProvysConnectionLabelingCallback.PROPERTY_USER + "=1000000000000098",
+            ProvysConnectionLabelingCallback.USER_MATCH}
+        , new Object[]{ProvysConnectionLabelingCallback.PROPERTY_USER + "=1000000000000098",
+            ProvysConnectionLabelingCallback.PROPERTY_TYPE + '='
                 + ProvysConnectionLabelingCallback.CONNECTION_TOKEN + '\n'
-                        + ProvysConnectionLabelingCallback.PROPERTY_TOKEN + "=xsqjkhfweqiufhnejngfv\n"
-                        + ProvysConnectionLabelingCallback.PROPERTY_USER + "=1000000000000100", ProvysConnectionLabelingCallback.REUSE_TOKEN}
-                , new Object[]{ProvysConnectionLabelingCallback.PROPERTY_USER + "=1000000000000098", ProvysConnectionLabelingCallback.PROPERTY_TYPE + '='
+                + ProvysConnectionLabelingCallback.PROPERTY_TOKEN + "=xsqjkhfweqiufhnejngfv\n"
+                + ProvysConnectionLabelingCallback.PROPERTY_USER + "=1000000000000100",
+            ProvysConnectionLabelingCallback.REUSE_TOKEN}
+        , new Object[]{ProvysConnectionLabelingCallback.PROPERTY_USER + "=1000000000000098",
+            ProvysConnectionLabelingCallback.PROPERTY_TYPE + '='
                 + ProvysConnectionLabelingCallback.CONNECTION_USER + '\n'
-                        + ProvysConnectionLabelingCallback.PROPERTY_USER + "=1000000000000098", ProvysConnectionLabelingCallback.USER_MATCH}
-                , new Object[]{ProvysConnectionLabelingCallback.PROPERTY_USER + "=1000000000000098", ProvysConnectionLabelingCallback.PROPERTY_TYPE + '='
+                + ProvysConnectionLabelingCallback.PROPERTY_USER + "=1000000000000098",
+            ProvysConnectionLabelingCallback.USER_MATCH}
+        , new Object[]{ProvysConnectionLabelingCallback.PROPERTY_USER + "=1000000000000098",
+            ProvysConnectionLabelingCallback.PROPERTY_TYPE + '='
                 + ProvysConnectionLabelingCallback.CONNECTION_USER + '\n'
-                        + ProvysConnectionLabelingCallback.PROPERTY_USER + "=1000000000000100", ProvysConnectionLabelingCallback.REUSE_USER}
-        );
-    }
+                + ProvysConnectionLabelingCallback.PROPERTY_USER + "=1000000000000100",
+            ProvysConnectionLabelingCallback.REUSE_USER}
+    );
+  }
 
-    @ParameterizedTest
-    @MethodSource
-    void costTest(String reqLabelsString, String currentLabelsString, int result) {
-        try (
-                var reqLabelsReader = new StringReader(reqLabelsString);
-                var currentLabelsReader = new StringReader(currentLabelsString)) {
-            var reqLabels = new Properties();
-            reqLabels.load(reqLabelsReader);
-            var currentLabels = new Properties();
-            currentLabels.load(currentLabelsReader);
-            assertThat(new ProvysConnectionLabelingCallback().cost(reqLabels, currentLabels)).isEqualTo(result);
-        } catch (IOException e) {
-            throw new InternalException("Error reading labels", e);
-        }
+  @ParameterizedTest
+  @MethodSource
+  void costTest(String reqLabelsString, String currentLabelsString, int result) {
+    try (
+        var reqLabelsReader = new StringReader(reqLabelsString);
+        var currentLabelsReader = new StringReader(currentLabelsString)) {
+      var reqLabels = new Properties();
+      reqLabels.load(reqLabelsReader);
+      var currentLabels = new Properties();
+      currentLabels.load(currentLabelsReader);
+      assertThat(new ProvysConnectionLabelingCallback().cost(reqLabels, currentLabels))
+          .isEqualTo(result);
+    } catch (IOException e) {
+      throw new InternalException("Error reading labels", e);
     }
+  }
 
 }
